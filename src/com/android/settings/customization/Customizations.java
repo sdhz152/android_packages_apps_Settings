8f7bc5e2d727da954d47296149c5b45612facbaa
sdhz150 <yanghui140@gmail.com>
2018-04-02 14:03:02 +0800

    Revert "Quick settings panel : Set transparency [2/2]"
    This reverts commit cae98b5f12c8f7b0cf925730ea7d81789dd263c8.

--- src/com/android/settings/customization/Customizations.java
+++ src/com/android/settings/customization/Customizations.java
@@ -16,63 +16,25 @@
 
 package com.android.settings.customization;
 
-import android.content.Context;
-import android.content.ContentResolver;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.provider.Settings;
-
-import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceCategory;
-import android.support.v7.preference.PreferenceScreen;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v14.preference.SwitchPreference;
-import com.android.settings.preferences.CustomSeekBarPreference;
-
+import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
-public class Customizations extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
- 
+public class Customizations extends SettingsPreferenceFragment {
+
     private static final String TAG = Customizations.class.getSimpleName();
-
-    private static final String QS_PANEL_ALPHA = "qs_panel_alpha";
-
-    private CustomSeekBarPreference mQsPanelAlpha;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.customizations);
-
-        final ContentResolver resolver = getActivity().getContentResolver();
-
-        mQsPanelAlpha = (CustomSeekBarPreference) findPreference(QS_PANEL_ALPHA);
-        int qsPanelAlpha = Settings.System.getIntForUser(resolver,
-                Settings.System.QS_PANEL_BG_ALPHA, 255, UserHandle.USER_CURRENT);
-        mQsPanelAlpha.setValue(qsPanelAlpha);
-        mQsPanelAlpha.setOnPreferenceChangeListener(this);
-
+        Preference prefSet = getPreferenceScreen();
     }
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.DISPLAY;
+        return MetricsProto.MetricsEvent.XOS;
     }
-
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mQsPanelAlpha) {
-            int bgAlpha = (Integer) newValue;
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.QS_PANEL_BG_ALPHA, bgAlpha,
-                    UserHandle.USER_CURRENT);
-            return true;
-        }
-        return false;
-     }
-
 }
